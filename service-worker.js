1ba9d0ee8a9b1cffc1f07f3c8a2993683e7f9cbf
Kakashy <josephwawerug@gmail.com>
2021-12-30 17:20:03 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,26 @@
-const timestamp = 1640873082801;
+const timestamp = 1640873979189;
 const build = [
-  "/_app/start-4126571c.js",
-  "/_app/assets/start-61d1577b.css",
-  "/_app/pages/__layout.svelte-55f12a54.js",
-  "/_app/assets/pages/__layout.svelte-2b7db775.css",
-  "/_app/error.svelte-119c7c79.js",
-  "/_app/pages/index.svelte-f28dd275.js",
-  "/_app/assets/pages/index.svelte-6dd47286.css",
-  "/_app/pages/anime.svelte-b4c4d305.js",
-  "/_app/assets/pages/anime.svelte-fba01625.css",
-  "/_app/pages/menu.svelte-522e62b6.js",
-  "/_app/chunks/vendor-1ce14ce5.js"
+  "/dishi/internal/start-ae272efb.js",
+  "/dishi/internal/assets/start-61d1577b.css",
+  "/dishi/internal/pages/__layout.svelte-5a6a7357.js",
+  "/dishi/internal/assets/pages/__layout.svelte-2b7db775.css",
+  "/dishi/internal/error.svelte-119c7c79.js",
+  "/dishi/internal/pages/index.svelte-f28dd275.js",
+  "/dishi/internal/assets/pages/index.svelte-6dd47286.css",
+  "/dishi/internal/pages/anime.svelte-b4c4d305.js",
+  "/dishi/internal/assets/pages/anime.svelte-fba01625.css",
+  "/dishi/internal/pages/menu.svelte-522e62b6.js",
+  "/dishi/internal/chunks/vendor-1ce14ce5.js"
 ];
 const files = [
-  "/.nojekyll",
-  "/favicon.png",
-  "/fonts/A Bug s Life - Debugged.ttf",
-  "/fonts/NewsGoth Lt BT Light.ttf",
-  "/media/adalia.jpg",
-  "/media/adalia2.jpg",
-  "/media/dan.jpg",
-  "/media/stil.jpg"
+  "/dishi/.nojekyll",
+  "/dishi/favicon.png",
+  "/dishi/fonts/A Bug s Life - Debugged.ttf",
+  "/dishi/fonts/NewsGoth Lt BT Light.ttf",
+  "/dishi/media/adalia.jpg",
+  "/dishi/media/adalia2.jpg",
+  "/dishi/media/dan.jpg",
+  "/dishi/media/stil.jpg"
 ];
 const applicationCache = `applicationCache-v${timestamp}`;
 const staticCache = `staticCache-v${timestamp}`;
