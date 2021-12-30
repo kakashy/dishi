3b265df8a70a323697b5ac3f8c6197fb067941bd
kakashy <josephwawerug@gmail.com>
2021-12-30 17:15:49 +0300

    [fix] gh

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,6 +16,12 @@ const config = {
 			fallback: null,
 			precompress: false
 		}),
+		paths: {
+			base: dev ? '' : '/dishi',
+		},
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'internal', etc.
+		appDir: 'internal',
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
